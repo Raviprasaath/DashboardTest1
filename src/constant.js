1c6128742734eff85646a9de250315b4dd7971c9
Raviprasaath <raviprasaath1@gmail.com>
2024-03-02 09:32:17 +0530

    2

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:3000/eCommerceData';
-
+// export const BASE_URL = 'http://localhost:3000/eCommerceData';
+export const BASE_URL = 'https://dashboard-data-base.onrender.com/eCommerceData';
